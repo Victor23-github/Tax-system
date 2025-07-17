a885f356c37e11b5bc47b69525cd7fc025a9d5ae
Victor <mr.victorunwana@gmail.com>
2025-07-17 21:54:18 +0100

    Install mysql2 package for Sequelize

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,14 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
-import router from './api/routes/taxpayerRoutes.js';
+import router from './routes/taxpayerRoutes.js';
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json()); // For parsing application/json
 app.use(express.urlencoded({ extended: false })); // For parsing application/x-www-form-urlencoded
-app.use(cors());
+app.use(
+  cors({
+    origin: 'https://tax-system-project.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
 
 app.use('/api', router);
 
